59b179ee2f076362feefac19b00f0aa43380518b
TylerCaris <tcaris@calpoly.edu>
2025-05-16 15:51:26 -0700

    added comes_before

--- bst.py
+++ bst.py
@@ -5,5 +5,17 @@ from dataclasses import dataclass
 sys.setrecursionlimit(10**6)
 
 
+def comes_before(a: Any, b: Any) -> bool:
+    return a < b
+
 def example_fun(x : int) -> bool:
     return x < 142
+
+
+
+
+class Tests(unittest.TestCase):
+    pass
+
+if (__name__ == '__main__'):
+    unittest.main()
