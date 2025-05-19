cf7a1c085ee8550913b85e716c3dcfe1f12770e7
Esteban Perez <epere102@calpoly.edu>
2025-05-19 15:56:48 -0700

    added insert

--- bst.py
+++ bst.py
@@ -12,9 +12,32 @@ class Node:
     left: BinTree
     right: BinTree
 
+@dataclass(frozen=True)
+class frozenBinarySearchTree:
+    Tree: BinTree
+    comes_before: Callable[[Node,Node],bool]
 
-def comes_before(a: Any, b: Any) -> bool:
-    return a < b
+
+def is_empty(fbst:frozenBinarySearchTree)-> bool:
+    if fbst.Tree == None:
+        return True
+    else: return False
+    
+def insert(fbst:frozenBinarySearchTree,val:Any)->frozenBinarySearchTree:
+    bin = insert_helper(fbst.Tree,val,fbst.comes_before)
+    return frozenBinarySearchTree(bin,fbst.comes_before)
+
+def insert_helper(bt:BinTree,val:Any,function:Callable[[Node,Node],bool])->BinTree:
+    match bt:
+        case None:
+            return None
+        case Node(v,l,r):
+            if function(v,val):
+                return insert_helper(r)
+            else: return insert_helper(l)
+
+
+
 
 def example_fun(x : int) -> bool:
     return x < 142
